ef0cb6b2ad74a87c848252b82f37f1e9726e2e63
André Csillaghy <csillag@heliotrope.fritz.box>
2019-08-17 14:23:16 +0200

    Version for any number of LEDs

--- arduino/GOESFlareWatch/GOESFlareWatch.ino
+++ arduino/GOESFlareWatch/GOESFlareWatch.ino
@@ -1,28 +1,21 @@
 /*
-  Blink
+  GOESFlareWatch4LED - Version with 4 LEDs
 
-  Turns an LED on for one second, then off for one second, repeatedly.
+  This program receives through the serial interface
+  data from its brother Python program about the size of 
+  the current solar activity: 1, 2, 3, and 4.
 
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
+  The program in python is called the same, and is in an jupyter notebook.
 
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/Blink
+  Author: Andre Csillaghy, raumschiff.org
+  August 2019
+  
+  (Origin is the program BLIK.)
+  
 */
 
 // the setup function runs once when you press reset or power the board
+
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(13, OUTPUT);
@@ -46,22 +39,25 @@ void loop() {
         digitalWrite(13, LOW);
         delay(1000);                       // wait for a second
       } else if ( inByte == '1') {
-        digitalWrite(10, LOW);   // turn the LED on (HIGH is the voltage level)
+        digitalWrite(10, HIGH);   // turn the LED on (HIGH is the voltage level)
         digitalWrite(11, HIGH);
         digitalWrite(12, LOW);
         digitalWrite(13, LOW);
       } else if ( inByte == '2') {
-        digitalWrite(10, LOW);   
-        digitalWrite(11, LOW);
+        digitalWrite(10, HIGH);   
+        digitalWrite(11, HIGH);
         digitalWrite(12, HIGH);
         digitalWrite(13, LOW);
-
       } else if ( inByte == '3') {
-        digitalWrite(10, LOW);   
-        digitalWrite(11, LOW);
-        digitalWrite(12, LOW);
+        digitalWrite(10, HIGH);   
+        digitalWrite(11, HIGH);
+        digitalWrite(12, HIGH);
+        digitalWrite(13, HIGH);
+      } else {
+        digitalWrite(10, HIGH);   
+        digitalWrite(11, HIGH);
+        digitalWrite(12, HIGH);
         digitalWrite(13, HIGH);
       }
     }
 }
-
